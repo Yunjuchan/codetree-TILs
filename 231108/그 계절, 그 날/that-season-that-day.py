18021faddf7e5fabd64d2bc1fc7a0d631f6de9e0
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-11-08 09:36:48 +0900

    Title: 그 계절, 그 날, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231108/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,20 @@
+Y , M, D = map(int, input().split())
+def isYun(y) :
+    if y % 400 == 0 or (y % 100 != 0 and y % 4 == 0) :
+        return True
+    return False
+
+days = [31, 29 if isYun(Y) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+def printWeather(m, d) :
+    if m < 1 or m > 12 : return -1
+    elif d < 1 or d > days[m] :
+        return -1
+
+    if 1 <= m < 3 or m == 12 :
+        return 'Winter'
+    elif m < 6 : return 'Spring'
+    elif m < 9 : return 'Summer'
+    else : return 'Fall'
+
+result = printWeather(M, D)
+print(result)
