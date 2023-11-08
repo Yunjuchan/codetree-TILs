80040403c7772d40265c8f9d1766ecbc7ab89294
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-11-08 09:45:11 +0900

    Title: 그 계절, 그 날, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 231108/그 계절, 그 날/that-season-that-day.py
+++ 231108/그 계절, 그 날/that-season-that-day.py
@@ -3,11 +3,9 @@ def isYun(y) :
     if y % 400 == 0 or (y % 100 != 0 and y % 4 == 0) :
         return True
     return False
-
 days = [31, 29 if isYun(Y) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def printWeather(m, d) :
-    if m < 1 or m > 12 : return -1
-    elif d < 1 or d > days[m] :
+    if d > days[m-1] :
         return -1
 
     if 1 <= m < 3 or m == 12 :
