d52cabaf3620aabafdc7e4aa1e29e3f26760d584
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-11-08 11:58:07 +0900

    Title: 양수 직사각형의 최대 크기, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231108/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 231108/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,18 +1,30 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[[0, 0] for _ in range(m+1)] for _ in range(n+1)]
-for i in range(n+1) :
-    dp[i][0] = [i, 0]
-for j in range(m+1) :
-    dp[0][j] = [0, j]
+dp1 = [[0 for _ in range(m+1)] for _ in range(n+1)]
+dp2 = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
 Max = 0
 for i in range(n) :
     for j in range(m) :
         if arr[i][j] < 1 : continue
-        dp[i+1][j+1] = [min(dp[i][j][0], dp[i][j+1][0])+1, min(dp[i][j][1], dp[i+1][j][1])+1]
-        if Max < dp[i+1][j+1][0] * dp[i+1][j+1][1] :
-            Max = dp[i+1][j+1][0] * dp[i+1][j+1][1]
-if Max == 0 :
-    print(-1)
-else :
-    print(Max)
+        # 상하 연속
+        dp1[i+1][j+1] = dp1[i][j+1]+1
+        # 좌우 연속
+        dp2[i+1][j+1] = dp2[i+1][j]+1
+        t_y = dp1[i+1][j+1]
+        t_x = dp2[i+1][j+1]
+        for x in range(j+1, 0, -1) :
+            if t_y > dp1[i+1][x] :
+                t_y = dp1[i+1][x]
+            if t_y == 0 : break
+            if Max < t_y * (j-x+2) :
+                Max = t_y * (j-x+2)
+                
+        for y in range(i+1, 0, -1) :
+            if t_x > dp2[y][j+1] :
+                t_x = dp2[y][j+1]
+            if t_x == 0 : break
+            if Max < t_x * (i-y+2) :
+                Max = t_x * (i-y+2)
+
+print(Max)
