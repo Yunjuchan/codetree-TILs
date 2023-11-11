60f734d0f56401d37fc3394f83ee9023f1e82ec4
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-11-11 17:06:21 +0900

    Title: 정수 명령 처리 5, Time: 245ms, Memory: 28MB, Status: Passed - CodeTree

--- 231111/정수 명령 처리 5/process-numeric-commands-5.py
+++ 231111/정수 명령 처리 5/process-numeric-commands-5.py
@@ -1,12 +1,14 @@
 A = []
-N = int(input())
-for _ in range(N) :
+length = 0
+for _ in range(int(input())) :
     c, *x = input().split()
     if c == 'push_back' :
         A += x
+        length += 1
     elif c == 'get' :
         print(A[int(x[0])-1])
     elif c == 'size' :
-        print(len(A))
+        print(length)
     else :
         A.pop()
+        length -= 1
