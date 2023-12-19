4a7f261d613d57ce7287138250e2fa17b25d2e02
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-12-19 16:05:26 +0900

    Title: 오목의 승패, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231219/오목의 승패/concave-victory-and-defeat.py
+++ 231219/오목의 승패/concave-victory-and-defeat.py
@@ -3,6 +3,8 @@ def check(y, x, tp) :
     for i in range(3) :
         level = 1
         dy, dx = y, x
+        if arr[y][x] == arr[y-direct_y[i]][x-direct_x[i]] :
+            continue
         while True :
             dy += direct_y[i]
             dx += direct_x[i]
@@ -32,45 +34,3 @@ if result :
     print(*result[1:])
 else :
     print(0)
-
-# print(check(1, 2, 1))
-
-# N, tp = map(int, input().split())
-# visited = [False] * 7
-
-# def f1(level, N, path) :
-#     if level == N : 
-#         print(*path)
-#         return
-
-#     for i in range(1,7) :
-#         f1(level+1, N, path+[i])
-
-
-# def f2(level, N, start, path) :
-#     if level == N :
-#         print(*path)
-#         return
-
-#     for i in range(start, 7) :
-#         f2(level+1, N, i, path+[i])
-
-
-# def f3(level, N, path) :
-#     if level == N :
-#         print(*path)
-#         return
-
-#     for i in range(1, 7) :
-#         if visited[i] : continue
-#         visited[i] = True 
-#         f3(level+1, N, path+[i])
-#         visited[i] = False
-
-
-# if tp == 1 :
-#     f1(0, N, [])
-# elif tp == 2 :
-#     f2(0, N, 1, [])
-# elif tp == 3 :
-#     f3(0, N, [])
