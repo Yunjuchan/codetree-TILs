4d836bc668a8727cffe5f4783dab3040cbad27fc
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-12-19 15:32:43 +0900

    Title: 오목의 승패, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231219/오목의 승패/concave-victory-and-defeat.py
@@ -0,0 +1,33 @@
+def check(y, x, tp) :
+    
+    for i in range(3) :
+        level = 1
+        dy, dx = y, x
+        while True :
+            dy += direct_y[i]
+            dx += direct_x[i]
+            if dy > 18 or dx > 18 : break
+            if arr[dy][dx] == tp :
+                level += 1
+            else : break
+        if level == 5 :
+            return True
+    return False
+
+result = False
+direct_y = [1,0,1]
+direct_x = [0,1,1]
+arr = [list(map(int, input().split())) for _ in range(19)]
+for i in range(19) :
+    for j in range(19) :
+        if arr[i][j] :
+            ret = check(i, j, arr[i][j])
+            result = [i+1, j+1, arr[i][j]]
+        if result : break
+    if result : break
+# print(result)
+if result :
+    print(result[2])
+    print(*result[:2])
+else :
+    print(0)
