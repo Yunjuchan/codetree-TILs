be22d9713e9972286bdaf99eff303ff9d5ab84d4
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2023-12-19 15:43:13 +0900

    Title: 주사위 던지기 1, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231219/주사위 던지기 1/roll-dice-1.py
@@ -0,0 +1,38 @@
+N, tp = map(int, input().split())
+visited = [False] * 7
+
+def f1(level, N, path) :
+    if level == N : 
+        print(*path)
+        return
+
+    for i in range(1,7) :
+        f1(level+1, N, path+[i])
+
+
+def f2(level, N, start, path) :
+    if level == N :
+        print(*path)
+        return
+
+    for i in range(start, 7) :
+        f2(level+1, N, i, path + [i])
+
+
+def f3(level, N, path) :
+    if level == N :
+        print(*path)
+        return
+
+    for i in range(1, 7) :
+        if visited[i] : continue
+        visited[i] = True 
+        f3(level+1, N, path+[i])
+
+
+if tp == 1 :
+    f1(0, N, [])
+elif tp == 1 :
+    f2(0, N, 1, [])
+elif tp == 1 :
+    f3(0, N, [])
